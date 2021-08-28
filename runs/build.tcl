38a37acdf58b94fd19fae49be2fac538131ee0bc
Joseph Vincent <78327422+JPierceVincent@users.noreply.github.com>
2021-08-28 15:41:56 -0700

    Update build.tcl

--- runs/build.tcl
+++ runs/build.tcl
@@ -1,8 +1,23 @@
 # filename: build.tcl
 
+set BUILD_DATE [ clock format [ clock seconds ] -format %m%d%Y ]
+set BUILD_TIME [ clock format [ clock seconds ] -format %H%M%S ]
+#######################################################################################
+# User Settings 
+#######################################################################################
+
+# global settings
+set PROJ_NM "led_test_nonProject"
+set PROJ_DIR "./$PROJ_NM"
+set PART_NM "xczu9eg-ffvb1156-2-e"
+
+
+#######################################################################################
+# Build Design
+###############################################################
 # Assign part to in-memory project (will also create the in-memory project)
 # Used when generating ip and executing synth, impl.
-set_part "xcku060-ffva1517-2-i"
+set_part $PART_NM
 
 # read all design files
 read_vhdl ../rtl/top_level.vhd
@@ -22,6 +37,15 @@ place_design
 # Route Design
 route_design
 
+#Put WNS as an indicator for timing in the filename
+set WNS [get_property SLACK [get_timing_paths -max_paths 1 -nworst 1 -setup]]
+puts "Post Route WNS = $WNS"
+
 # Write out bitfile
-write_debug_probes -force led_test_nonProject.ltx
-write_bitstream -force led_test_nonProject.bit
+
+write_debug_probes -force $PROJ_DIR/${PROJ_NM}_${BUILD_DATE}_${BUILD_TIME}_${WNS}ns.ltx
+write_bitstream -force $PROJ_DIR/${PROJ_NM}_${BUILD_DATE}_${BUILD_TIME}_${WNS}ns.bit \
+ -bin_file
+
+#write_debug_probes -force led_test_nonProject.ltx
+#write_bitstream -force led_test_nonProject.bit
